82083cfc96472cb12727943307c8486e33612e28
samantha-marsee <sam572@cornell.edu>
2025-03-05 15:59:18 +0000

    renamed

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import AccordionPanel from './AccordionPanel'
 
 export default function Accordion() {
-  const panels = [
+  const accordionData = [
     { title: "About", content: <p>With a population of about 2 million, Almaty is Kazakhstan&apos;s largest city. From 1929 to 1997, it was its capital city.</p> },
     { title: "Etymology", content: <p>The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for &ldquo;apple&rdquo; and is often translated as &ldquo;full of apples&rdquo;. In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.</p> }
   ]
